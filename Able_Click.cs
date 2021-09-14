b65c499403ac029f7214c5f3b70a4bca00f95c7f
bibidong <67782053+bibidong@users.noreply.github.com>
2021-09-14 14:49:09 +0800

    测试上传
    上传啦

---
+++ Able_Click.cs
@@ -0,0 +1,26 @@
+﻿/***********************************
+*    Description: set 和 get 挂载对象可否点击
+*    Mountpoint：挂载点为需要添加该属性的对象
+***********************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Able_Click : MonoBehaviour
+{
+    public bool _clickable = false;
+
+    public bool Clickable
+    {
+        get
+        {
+            return _clickable;
+        }
+        set
+        {
+            _clickable = value;
+        }
+
+    }
+}
